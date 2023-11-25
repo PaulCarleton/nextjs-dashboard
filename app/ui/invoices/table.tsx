02d60efd8b5556e1862f22ac0fe7635fc30d862d
Paul Carleton <89902102+PaulCarleton@users.noreply.github.com>
2023-11-25 14:51:16 +0700

    added image alt text

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`${invoice.name}'s profile picture`} // meaningful text
                       />
                       <p>{invoice.name}</p>
                     </div>
